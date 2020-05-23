15cbfd7c9bcae526438c1628d5b0f6c536bfeff2
Lihuel Almada <Lihuel.Almada@gmail.com>
2020-05-23 18:25:57 -0300

    second try to css

--- js/app.js
+++ js/app.js
@@ -1,5 +1,34 @@
-function sendForm() {
+/*var name = document.getElementById('name');
+var lastName = document.getElementById('lastName');
+var email = document.getElementById('email');
+var age = document.getElementById('age');
+
+var error = document.getElementById('error');
+*/
+
+/*function sendForm() {
     console.log('Enviando formulario...');
 
+    var messageError = [];
+
+    if (name.value.length < 3) {
+        messageError.push('El nombre de usuario debe tener al menos 3 caracteres');
+    }
+    error.innerHTML = messageError.join(', ');
+
     return false;
-}
+}*/
+
+
+/*var form = document.getElementById('form');
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    console.log('Enviando Formulario...');
+    var messageError = [];
+
+    if (name.value === null || name.value === '') {
+        messageError.push('El nombre de usuario debe tener al menos 3 caracteres');
+        alert("Holi");
+    }
+    error.innerHTML = messageError.join(', ');
+});*/
