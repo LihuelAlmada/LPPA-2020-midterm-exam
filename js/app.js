9a3e8ed5cb8de44b8b979f0d15755b43d69f5464
Lihuel Almada <Lihuel.Almada@gmail.com>
2020-05-25 22:53:10 -0300

    validate name and lastName

--- js/app.js
+++ js/app.js
@@ -1,34 +1,40 @@
-/*var name = document.getElementById('name');
-var lastName = document.getElementById('lastName');
-var email = document.getElementById('email');
-var age = document.getElementById('age');
+//Make with https://www.youtube.com/watch?v=uUJr5Itz8kY&t=1404s
+//https://www.youtube.com/watch?v=j3ixg2cPI54
+//https://www.w3resource.com/javascript/form/email-validation.php
 
-var error = document.getElementById('error');
-*/
-
-/*function sendForm() {
-    console.log('Enviando formulario...');
-
-    var messageError = [];
-
-    if (name.value.length < 3) {
-        messageError.push('El nombre de usuario debe tener al menos 3 caracteres');
+function validate() {
+    if (userName.value.length < 3 && userName.value.length != 0) {
+        messageError.push('El nombre debe tener al menos 3 caracteres')
+        console.log('Nombre corto');
+    } else if (userName.value === null || userName.value === '') {
+        messageError.push('Complete el campo Nombre')
+        console.log('Nombre sin completar');
+    } else {
+        console.log(userName.value);
     }
-    error.innerHTML = messageError.join(', ');
+    if (lastName.value.length < 3 && lastName.value.length != 0) {
+        messageError.push('El apellido debe tener al menos 3 caracteres')
+        console.log('Nombre corto');
+    } else if (lastName.value === null || lastName.value === '') {
+        messageError.push('Complete el campo Apellido')
+        console.log('Apellido sin completar');
+    }
+}
 
+sendForm = function() {
+    console.log("from");
+    validate();
+    error.innerHTML = messageError.join('<br>');
     return false;
-}*/
-
-
-/*var form = document.getElementById('form');
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    console.log('Enviando Formulario...');
-    var messageError = [];
-
-    if (name.value === null || name.value === '') {
-        messageError.push('El nombre de usuario debe tener al menos 3 caracteres');
-        alert("Holi");
-    }
-    error.innerHTML = messageError.join(', ');
-});*/
+}
+window.onload = function() {
+    userName = document.getElementById('name');
+    lastName = document.getElementById('lastName');
+    email = document.getElementById('email');
+    age = document.getElementById('age');
+    sex = document.getElementById('sex');
+    error = document.getElementById('error');
+    messageError = [];
+    sendBtn = document.getElementById('send');
+    sendBtn.onclick = sendForm
+}
